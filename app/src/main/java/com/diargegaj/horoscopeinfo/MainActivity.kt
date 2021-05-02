b34712c744c46f8d36c629b4581616c635c435e7
Diar Gegaj <diar.gegaj@solaborate.com>
2021-05-02 23:36:24 +0200

    setup signsName recyclerView

--- app/src/main/java/com/diargegaj/horoscopeinfo/MainActivity.kt
+++ app/src/main/java/com/diargegaj/horoscopeinfo/MainActivity.kt
@@ -2,10 +2,42 @@ package com.diargegaj.horoscopeinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.diargegaj.horoscopeinfo.adapter.SignsTitleAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = SignsTitleAdapter()
+        adapter.setSigns(getAllSigns())
+
+        signsRecyclerView.layoutManager = LinearLayoutManager(this)
+        signsRecyclerView.adapter = adapter
+    }
+
+    private fun getAllSigns(): List<String> {
+        val signsNames = mutableListOf<String>()
+
+        signsNames.add("Aries")
+        signsNames.add("Taurus")
+        signsNames.add("Gemini")
+        signsNames.add("Cancer")
+        signsNames.add("Leo")
+        signsNames.add("Virgo")
+        signsNames.add("Libra")
+        signsNames.add("Scorpio")
+        signsNames.add("Sagittarius")
+        signsNames.add("Capricorn")
+        signsNames.add("Aquarius")
+        signsNames.add("Pisces")
+
+        return signsNames
     }
 }
---
+++ app/src/main/java/com/diargegaj/horoscopeinfo/adapter/SignsTitleAdapter.kt
@@ -0,0 +1,41 @@
+package com.diargegaj.horoscopeinfo.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.diargegaj.horoscopeinfo.R
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_signs_title.view.*
+
+/**
+ * Created by Diar Gegaj on 21-05-02.
+ */
+class SignsTitleAdapter: RecyclerView.Adapter<SignsTitleAdapter.SignsTitleViewHolder>() {
+
+    private var signs: List<String> = mutableListOf()
+
+    fun setSigns(signs: List<String>){
+        this.signs = signs
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SignsTitleViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_signs_title, parent, false)
+        return SignsTitleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SignsTitleViewHolder, position: Int) {
+        holder.fillView(signs[position])
+    }
+
+    override fun getItemCount(): Int {
+        return signs.size
+    }
+
+    inner class SignsTitleViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun fillView(signName: String) {
+            containerView.signTitle.text = signName
+        }
+    }
+}
